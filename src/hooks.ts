2f67a14d99eb152154e610d4724bd7aeba7bb44c
Mufassir Ahmad Chowdhury <mac22214u@gmail.com>
2024-11-09 01:25:17 +0600

    removed hooks.server.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,2 +1,8 @@
 import { i18n } from '$lib/i18n';
 export const reroute = i18n.reroute();
+
+// hooks.server.ts
+// import type { Handle } from '@sveltejs/kit';
+// import { i18n } from '$lib/i18n';
+// const handleParaglide: Handle = i18n.handle();
+// export const handle: Handle = handleParaglide;
